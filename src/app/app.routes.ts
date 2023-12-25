68b1da3042f1e2257109a86622083ef00565731e
Vlad Valean <vlad.valean19@gmail.com>
2023-12-26 00:03:41 +0200

    routes & navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {AboutUsPageComponent} from "./pages/about-us-page/about-us-page.component";
+import {ContactPageComponent} from "./pages/contact-page/contact-page.component";
+import {NotFoundPageComponent} from "./pages/not-found-page/not-found-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', title: 'Home', component: HomePageComponent },
+  { path: 'about-us', title: 'About us', component: AboutUsPageComponent },
+  { path: 'contact-us', title: 'Contact us', component: ContactPageComponent },
+  { path: '**', title: 'Error 404', component: NotFoundPageComponent}
+];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NgForOf
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  links:{name: string, path: string}[] = [
+    {name: 'Home', path: '/'},
+    {name: 'About us', path: '/about-us'},
+    {name: 'Contact', path: '/contact-us'}
+  ]
 }
